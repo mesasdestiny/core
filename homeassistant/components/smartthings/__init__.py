9b57703d736984b31d9b952531e197f83b3c8f95
mesasdestiny <mesas.destiny.0p@icloud.com>
2025-04-02 18:18:24 +0000

    Refactoring to use CAPABILITY_TO_SWITCHES to add the lighting and beep switches

--- homeassistant/components/smartthings/__init__.py
+++ homeassistant/components/smartthings/__init__.py
@@ -469,6 +469,12 @@ KEEP_CAPABILITY_QUIRK: dict[
     Capability.WASHER_OPERATING_STATE: (
         lambda status: status[Attribute.SUPPORTED_MACHINE_STATES].value is not None
     ),
+    Capability.SAMSUNG_CE_AIR_CONDITIONER_LIGHTING: (
+        lambda status: status[Attribute.SUPPORTED_LIGHTING_LEVELS].value is not None
+    ),
+    Capability.SAMSUNG_CE_AIR_CONDITIONER_BEEP: (
+        lambda status: status[Attribute.BEEP].value is not None
+    ),
     Capability.DEMAND_RESPONSE_LOAD_CONTROL: lambda _: True,
 }
 
